178ead7a501e4ed8255ef4f49972b766b898c966
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-11-05 01:00:49 +0100

    Add files via upload

--- package/features.py
+++ package/features.py
@@ -5,7 +5,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from scipy import stats
 
-from package import hr, smoothness
+from semio_package import hr, smoothness
 
 
 # --------------------------------------
@@ -431,11 +431,11 @@ def get_stride_list(data_tronc, seg_lim, steps_lim, freq=100):
 
     steps_lim = steps_lim.sort_values(by="HS")
     for i in range(1, len(steps_lim) - 4): # On ne prend pas en compte les 2 derniers pas qui peuvent être freinés
-        t_tot = steps_lim.iloc[i + 2, 4] - steps_lim.iloc[i, 4]
-        if ((steps_lim.iloc[i, 0] + steps_lim.iloc[i + 1, 0] == 1)
-                & (steps_lim.iloc[i + 1, 0] + steps_lim.iloc[i + 2, 0] == 1)):
+        t_tot = steps_lim["HS"][i + 2] - steps_lim["HS"][i]
+        if ((steps_lim["Foot"][i] + steps_lim["Foot"][i + 1] == 1)
+                & (steps_lim["Foot"][i + 1] + steps_lim["Foot"][i + 2] == 1)):
 
-            if inside([steps_lim.iloc[i, 4], steps_lim.iloc[i + 2, 4]], seg_lim):
+            if inside([steps_lim["HS"][i], steps_lim["HS"][i + 2]], seg_lim):
                 t.append(t_tot)
 
     t = rmoutliers(t)
@@ -451,14 +451,14 @@ def get_double_stance_time_list(data_tronc, seg_lim, steps_lim, freq=100):
 
     steps_lim = steps_lim.sort_values(by="HS")
     for i in range(1, len(steps_lim) - 4):
-        st1 = (steps_lim.iloc[i + 2, 3] - steps_lim.iloc[i + 1, 4])
-        st2 = (steps_lim.iloc[i + 1, 3] - steps_lim.iloc[i, 4])
-        t_tot = steps_lim.iloc[i + 2, 4] - steps_lim.iloc[i, 4]
+        st1 = (steps_lim["TO"][i + 2] - steps_lim["HS"][i + 1])
+        st2 = (steps_lim["TO"][i + 1] - steps_lim["HS"][i])
+        t_tot = steps_lim["HS"][i + 2] - steps_lim["HS"][i]
         st = (st1 + st2) / t_tot
-        if ((steps_lim.iloc[i, 0] + steps_lim.iloc[i + 1, 0] == 1) &
-            (steps_lim.iloc[i + 1, 0] + steps_lim.iloc[i + 2, 0] == 1)) & (st1 > 0) & (st2 > 0):
+        if ((steps_lim["Foot"][i] + steps_lim["Foot"][i + 1, 0] == 1) &
+            (steps_lim["Foot"][i + 1] + steps_lim["Foot"][i + 2, 0] == 1)) & (st1 > 0) & (st2 > 0):
 
-            if inside([steps_lim.iloc[i, 4], steps_lim.iloc[i + 2, 4]], seg_lim):
+            if inside([steps_lim["HS"][i], steps_lim["HS"][i + 2]], seg_lim):
                 dst_t.append(st)
 
     dst_t = rmoutliers(dst_t)
