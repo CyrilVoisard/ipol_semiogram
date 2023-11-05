76111921069b9cb875f506ade14b3ebac22dc42d
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-11-05 15:26:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ if __name__ == "__main__":
                             help='Reference set - Time series for the lower back sensor.', required=False)
         parser.add_argument('-i3', metavar='ref_gait_events',
                             help='Reference set - Metadata with gait events.', required=False)
-        compare = True
+        compare = False
     except: 
         compare = False
         
