c8fabf4e280dcb92a7deb3b8ed3b9e5fbee50594
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-11-05 16:53:11 +0100

    Update hr.py

--- package/hr.py
+++ package/hr.py
@@ -7,56 +7,34 @@ from scipy import stats
 from package import features as ft
 
 
-def DFT(x, fs=100):
-    # Function to calculate the discrete Fourier Transform coefficient à partir de la fréquence fondamentale
-    # https://www.f-legrand.fr/scidoc/docimg/numerique/tfd/periodique2/periodique2.html
+def ihr_avg(seg_lim, steps_lim, s, ml=False):
+    # Pour ML, le rapport est inversé !
+    ihr_list = []
+    for j in range(1, len(steps_lim) - 2):  # on ne prend pas en compte les premier et le dernier pas
+        if steps_lim["Foot"].iloc[j] - steps_lim["Foot"].iloc[j + 2] == 0:  # on vérifie que c'est le même pied
+            if ft.inside([steps_lim["HS"].iloc[j], steps_lim["HS"].iloc[j + 2]], seg_lim):
+                # On est conscient que la détection de pas n'est pas fiable à 100%.
+                # On va donc prendre le maximum autour de la détection des pas.
+                if not ml:
+                    det = det_max(s, steps_lim["HS"].iloc[j], steps_lim["HS"].iloc[j+2])
+                else:
+                    det = det_max_ml(s, steps_lim["HS"].iloc[j], steps_lim["HS"].iloc[j + 2])
+                if det != 0:
+                    ihr_list.append(det)
 
-    f0 = 100 / len(x)
+    ihr_list = ft.rmoutliers(ihr_list)
 
-    peak_list = []
-    n_f0 = []
-
-    x = np.array(x.to_list())
-
-    N = len(x)
-    n = np.arange(21)
-    for k in n:
-        e = np.exp(-2j * np.pi * f0 * k * np.arange(len(x)) / fs)
-        peak = abs(sum(x * e))
-        peak_list.append(peak)
-        n_f0.append(k)
-
-    return peak_list
+    return np.mean(ihr_list), np.std(ihr_list)
 
 
-def ihr(sig):
-    peak_list = DFT(sig)
-
-    peak_pair_sum = 0
-    for peak in peak_list[2:21:2]:
-        peak_pair_sum = peak_pair_sum + peak * peak
-
-    peak_impair_sum = 0
-    for peak in peak_list[1:21:2]:
-        peak_impair_sum = peak_impair_sum + peak * peak
-
-    if (peak_impair_sum == 0) | (peak_impair_sum == 0):
-        return 0
-    else:
-        return 100 * peak_pair_sum / (peak_impair_sum + peak_pair_sum)
-
-
-def det_max(s, start, end, i):
+def det_max(s, start, end):
     # Pour déterminer le maximum du HR au voisinage des points de détection automatique
     det_list = []
     for k in range(30):
         for kk in range(5):
             # On fait varier le signal à son origine
             s_step = s[max(start - 15 + k, 0):end - 15 + k - 2 + kk]
-            if i == 0:
-                calcul = hr(s_step)
-            else:
-                calcul = ihr(s_step)
+            calcul = ihr(s_step)
             if calcul != 0:
                 det_list.append(calcul)
             else:
@@ -78,10 +56,7 @@ def det_max_ml(s, start, end, i):
         for kk in range(5):
             # On fait varier le signal à son origine
             s_step = s[max(start - 15 + k, 0):end - 15 + k - 2 + kk]
-            if i == 0:
-                calcul = 1 / hr(s_step)
-            else:
-                calcul = 100 - ihr(s_step)
+            calcul = 100 - ihr(s_step)
             if calcul != 0:
                 det_list.append(calcul)
             else:
@@ -94,25 +69,42 @@ def det_max_ml(s, start, end, i):
         return max(det_list)
     else:
         return 0
+        
 
+def ihr(sig):
+    peak_list = DFT(sig)
 
-def hr_ihr_moyen(seg_lim, steps_lim, s, i=1, ml=False):
-    # Pour ML, le rapport est inversé !
-    # Le i=1 signifie qu'on considère IHR et non pas HR
-    # Le i=0 signifie qu'on considère HR et non pas IHR
-    hr_ihr_list = []
-    for j in range(1, len(steps_lim) - 2):  # on ne prend pas en compte les premier et le dernier pas
-        if steps_lim["Foot"].iloc[j] - steps_lim["Foot"].iloc[j + 2] == 0:  # on vérifie que c'est le même pied
-            if ft.inside([steps_lim["HS"].iloc[j], steps_lim["HS"].iloc[j + 2]], seg_lim):
-                # On est conscient que la détection de pas n'est pas fiable à 100%.
-                # On va donc prendre le maximum autour de la détection des pas.
-                if not ml:
-                    det = det_max(s, steps_lim["HS"].iloc[j], steps_lim["HS"].iloc[j+2], i=i)
-                else:
-                    det = det_max_ml(s, steps_lim["HS"].iloc[j], steps_lim["HS"].iloc[j + 2], i=i)
-                if det != 0:
-                    hr_ihr_list.append(det)
+    peak_pair_sum = 0
+    for peak in peak_list[2:21:2]:
+        peak_pair_sum = peak_pair_sum + peak * peak
 
-    hr_ihr_list = ft.rmoutliers(hr_ihr_list)
+    peak_impair_sum = 0
+    for peak in peak_list[1:21:2]:
+        peak_impair_sum = peak_impair_sum + peak * peak
 
-    return np.mean(hr_ihr_list), np.std(hr_ihr_list)
+    if (peak_impair_sum == 0) | (peak_impair_sum == 0):
+        return 0
+    else:
+        return 100 * peak_pair_sum / (peak_impair_sum + peak_pair_sum)
+        
+
+def DFT(x, fs=100):
+    # Function to calculate the discrete Fourier Transform coefficient à partir de la fréquence fondamentale
+    # https://www.f-legrand.fr/scidoc/docimg/numerique/tfd/periodique2/periodique2.html
+
+    f0 = 100 / len(x)
+
+    peak_list = []
+    n_f0 = []
+
+    x = np.array(x.to_list())
+
+    N = len(x)
+    n = np.arange(21)
+    for k in n:
+        e = np.exp(-2j * np.pi * f0 * k * np.arange(len(x)) / fs)
+        peak = abs(sum(x * e))
+        peak_list.append(peak)
+        n_f0.append(k)
+
+    return peak_list
