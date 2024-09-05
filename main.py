e96ea010485cad7bb165c85cf989fd2fc75d8de6
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2024-09-05 19:27:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -189,7 +189,7 @@ if __name__ == "__main__":
     
     # load data (only lower back in this demo)
     try:
-        data_lb = import_data.import_XSens(os.path.join(data_WD, args.i0), freq)
+        data_lb = import_data.import_XSens(os.path.join(data_WD, args.i0))
         seg_lim = import_data.get_seg(os.path.join(data_WD, args.i1))
         steps_lim = import_data.get_steps(os.path.join(data_WD, args.i1), seg_lim)
     except Exception as e:
@@ -198,7 +198,7 @@ if __name__ == "__main__":
         sys.exit(0)
     if compare :
         try:
-            ref_data_lb = import_data.import_XSens(os.path.join(data_WD, args.i2), freq)
+            ref_data_lb = import_data.import_XSens(os.path.join(data_WD, args.i2))
             ref_seg_lim = import_data.get_seg(os.path.join(data_WD, args.i3))
             ref_steps_lim = import_data.get_steps(os.path.join(data_WD, args.i3), seg_lim)
         except Exception as e:
