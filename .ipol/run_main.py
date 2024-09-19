abdc9a50e7f9a99712f1f6aa2f3b29137df70f36
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2024-09-19 17:34:15 +0200

    Update run_main.py

--- .ipol/run_main.py
+++ .ipol/run_main.py
@@ -19,6 +19,7 @@ if os.path.isfile('input_2.txt') and os.path.isfile('input_3.json'):
     ref = "-i2 $input_2 -i3 $input_3"
 
 command = f"python $bin/main.py -i0 $input_0 -i1 $input_1 {ref} -freq $freq -distance $distance -min_z $min_z -max_z $max_z"
-"""
 command = "python $bin/main.py -i0 $input_0 -i1 $input_1 -i2 $input_2 -i3 $input_3 -freq $freq -distance $distance -min_z $min_z -max_z $max_z"
+"""
+command = "python $bin/main.py -i0 $input_0 -i1 $input_1 -freq $freq -distance $distance -min_z $min_z -max_z $max_z"
 subprocess.run(command, shell=True)
