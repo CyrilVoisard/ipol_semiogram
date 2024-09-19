fb96f0de5d2876340a3063dac6a76972cf1202f5
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2024-09-19 17:39:41 +0200

    Update run_main.py

--- .ipol/run_main.py
+++ .ipol/run_main.py
@@ -2,18 +2,6 @@ import argparse
 import os
 import subprocess
 
-# Create the parser
-parser = argparse.ArgumentParser(description="Process para.")
-
-# Add the arguments
-parser.add_argument('freq', type=str, help='The frequency')
-parser.add_argument('distance', type=str, help='The distance')
-parser.add_argument('min_z', type=str, help='The minimum z')
-parser.add_argument('max_z', type=str, help='The maximum z')
-
-# Parse the arguments
-args = parser.parse_args()
-
 ref = ""
 if os.path.isfile('input_2.txt') and os.path.isfile('input_3.json'):
     ref = "-i2 $input_2 -i3 $input_3"
