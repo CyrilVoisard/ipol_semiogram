d4dd609a17b2dfba7f657bb75c1e43c8edd86305
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2024-09-19 17:19:27 +0200

    Update run_main.py

--- .ipol/run_main.py
+++ .ipol/run_main.py
@@ -2,7 +2,7 @@ import argparse
 import os
 import subprocess
 
-# Create the parser
+"""# Create the parser
 parser = argparse.ArgumentParser(description="Process para.")
 
 # Add the arguments
@@ -19,5 +19,6 @@ if 1==1: # os.path.isfile('input_2.txt') and os.path.isfile('input_3.json'):
     ref = "-i2 $input_2 -i3 $input_3"
 
 command = f"python $bin/main.py -i0 input_0 -i1 $input_1 {ref} -freq $freq -distance $distance -min_z $min_z -max_z $max_z"
-
+"""
+command = "python $bin/main.py -i0 $input_0 -i1 $input_1 -i2 $input_2 -i3 $input_3 -freq $freq -distance $distance -min_z $min_z -max_z $max_z"
 subprocess.run(command, shell=True)
