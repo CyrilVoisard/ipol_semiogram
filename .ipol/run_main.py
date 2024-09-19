6eb6a1f7a23ad586b5ac1930007fc2aa09469e11
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2024-09-19 17:16:21 +0200

    Update run_main.py

--- .ipol/run_main.py
+++ .ipol/run_main.py
@@ -15,7 +15,7 @@ parser.add_argument('max_z', type=str, help='The maximum z')
 args = parser.parse_args()
 
 ref = ""
-if os.path.isfile('input_2.txt') and os.path.isfile('input_3.json'):
+if 1==1: # os.path.isfile('input_2.txt') and os.path.isfile('input_3.json'):
     ref = "-i2 $input_2 -i3 $input_3"
 
 command = f"python $bin/main.py -i0 input_0 -i1 $input_1 {ref} -freq $freq -distance $distance -min_z $min_z -max_z $max_z"
