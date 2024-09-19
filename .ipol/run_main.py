fdaeb8043e6b736e1b08a57c273a684cb7b2dece
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2024-09-19 17:03:15 +0200

    Update run_main.py

--- .ipol/run_main.py
+++ .ipol/run_main.py
@@ -1,16 +1,23 @@
+import argparse
 import os
 import subprocess
 
-# Arguments: freq, distance, min_z, max_z
-freq = input("Enter freq: ")
-distance = input("Enter distance: ")
-min_z = input("Enter min_z: ")
-max_z = input("Enter max_z: ")
+# Create the parser
+parser = argparse.ArgumentParser(description="Process some integers.")
+
+# Add the arguments
+parser.add_argument('freq', type=str, help='The frequency')
+parser.add_argument('distance', type=str, help='The distance')
+parser.add_argument('min_z', type=str, help='The minimum z')
+parser.add_argument('max_z', type=str, help='The maximum z')
+
+# Parse the arguments
+args = parser.parse_args()
 
 ref = ""
 if os.path.isfile('input_2.txt') and os.path.isfile('input_3.json'):
     ref = "-i3 input_2.txt -i4 input_3.json"
 
-command = f"python $bin/main.py -i0 input_0.txt -i1 $input_1 -i2 $input_2 {ref} -freq {freq} -distance {distance} -min_z {min_z} -max_z {max_z}"
+command = f"python $bin/main.py -i0 input_0.txt -i1 $input_1 -i2 $input_2 {ref} -freq {args.freq} -distance {args.distance} -min_z {args.min_z} -max_z {args.max_z}"
 
 subprocess.run(command, shell=True)
