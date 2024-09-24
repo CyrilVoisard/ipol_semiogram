fddb72bbc04a2c87b5e97d3fe2cc9e8cc86914e7
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2024-09-24 17:33:19 +0200

    Update run_main.py

--- .ipol/run_main.py
+++ .ipol/run_main.py
@@ -10,7 +10,7 @@ if os.path.isfile('input_2.txt') and os.path.isfile('input_3.json'):
     compare = 1
 
 command = f"python $bin/main.py -i0 $input_0 -i1 $input_1 {ref} -freq $freq -distance $distance -min_z $min_z -max_z $max_z"
-f = open('algo_info.txt','w')
+f = open('info.txt','w')
 f.write("ref = " + str(compare))
 f.close()
 
